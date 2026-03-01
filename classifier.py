439d9856aeb62613e68b4467488b73d036d2c5dc
DoriaA13 <doria.achouri13@gmail.com>
2026-03-01 10:56:54 +0100

    Ajouts stats, modularité + n-grammes de mots

--- classifier.py
+++ classifier.py
@@ -5,9 +5,22 @@ Simple language classifier using character n-grams.
 import sys
 from collections import Counter, defaultdict
 
-def extract_ngrams(text, n=3):
+def extract_char_ngrams(text, n=3):
     """Extract character n-grams from text."""
     return [text[i:i+n] for i in range(len(text)-n+1)]
+
+def extract_words_ngrams(text, n=3):
+    """Extract words n-grams from text."""
+    ngrams = []
+    sent = text.split(" ")
+    for i in range(len(sent) - n + 1):
+        current_ngram = tuple(sent[i:i+n])
+        ngrams.append(current_ngram)
+    return ngrams
+
+#########Parameters for ngram extraction
+ngram_func = extract_words_ngrams    #line to change to choose between characters and words n-grams
+n = 3
 
 def train(train_file):
     """Train: learn top n-grams for each language."""
@@ -18,7 +31,7 @@ def train(train_file):
             parts = line.strip().split('\t')
             if len(parts) == 2:
                 text, lang = parts
-                ngrams = extract_ngrams(text, n=3)
+                ngrams = ngram_func(text, n)
                 lang_ngrams[lang].update(ngrams)
     
     # Keep top 100 n-grams per language
@@ -30,7 +43,7 @@ def train(train_file):
 
 def predict(text, lang_features):
     """Predict language by counting matching n-grams."""
-    ngrams = set(extract_ngrams(text, n=3))
+    ngrams = set(ngram_func(text, n))
     scores = {}
     
     for lang, features in lang_features.items():
@@ -48,13 +61,28 @@ def main():
     
     # Train
     lang_features = train(train_file)
-    
+    lang_count = 0
+    lang_right = 0
+    false_sents = []
     # Predict
     with open(test_file, 'r', encoding='utf-8') as f:
         for line in f:
-            text = line.strip().split('\t')[0]
+            whole_tab = line.strip().split('\t')
+            text = whole_tab[0]
+            if test_file == "dev.txt" :    #if we use this corpus then we could obtain stats
+                true_lang = whole_tab[1]
+                lang_count+=1
             pred_lang = predict(text, lang_features)
-            print(f"{text}\t{pred_lang}")
-
+            
+            if (test_file == "dev.txt") :
+                print(f"{text}\t{pred_lang}\tvraie réponse : {true_lang}")
+                if pred_lang == true_lang:
+                    lang_right += 1
+                else : 
+                    false_sents.append((text, pred_lang, true_lang))
+            else :
+                print(f"{text}\t{pred_lang}")
+    system_score = (lang_right/lang_count) * 100
+    print("Le système développé a obtenu un score de ", system_score, "%.")
 if __name__ == '__main__':
     main()
